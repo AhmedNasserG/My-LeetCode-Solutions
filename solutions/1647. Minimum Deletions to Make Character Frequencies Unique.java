c2c23f4f93fb1453ad633d9385516bd6fb526690
Ahmed Nasser <ahmednasser217217@gmail.com>
2022-03-03 19:57:58 +0200

    12 ms

---
+++ solutions/1647. Minimum Deletions to Make Character Frequencies Unique.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minDeletions(String s) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        Arrays.sort(freq);
+        int keep = freq[25], prev = freq[25];
+        for (int i = 24; i >= 0 && freq[i] != 0 && prev != 0; i--) {
+            prev = Math.min(freq[i], prev - 1);
+            keep += prev;
+        }
+        return s.length() - keep;
+    }
+}
+​
