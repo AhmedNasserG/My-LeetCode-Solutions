7b6ee6705ec929c4233d6788fbbc5956e4fb7a0d
Ahmed Nasser <ahmednasser217217@gmail.com>
2022-05-19 12:43:18 +0200

    2 ms

--- solutions/662. Maximum Width of Binary Tree.java
+++ solutions/662. Maximum Width of Binary Tree.java
@@ -1,36 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    HashMap<Integer, Interval> map;
     public int widthOfBinaryTree(TreeNode root) {
-        map = new HashMap();
-        dfs(root, 0, 1);
-        int ans = 0;
-        for (Interval i : map.values()) {
-            ans = Math.max(ans, i.getWidth());
-        }
-        return ans;
+        return dfs(root, 0, 0, new ArrayList());
     }
     
-    private void dfs(TreeNode node, int level, int order) {
-        if (node == null) return;
-        map.putIfAbsent(level, new Interval(order));
-        map.get(level).updateInterval(order);
-        dfs(node.left, level + 1, 2 * order);
-        dfs(node.right, level + 1, 2 * order + 1);
-    }
-    
-    class Interval {
-        private int start, end;
-        Interval(int val) {
-            start = val;
-            end = val;
+    private int dfs(TreeNode node, int level ,int order, ArrayList<Integer> list) {
+        if (node == null) return 0;
+        if (level == list.size()) {
+            list.add(order);
         }
-        
-        void updateInterval(int val) {
-            end = val;
-        }
-        
-        int getWidth() {
-            return end - start + 1;    
-        }
+        int ans = order - list.get(level) + 1;
+        int left = dfs(node.left, level + 1, 2 * order, list);
+        int right = dfs(node.right, level + 1, 2 * order + 1, list);
+        return Math.max(ans, Math.max(left, right));
     }
 }
