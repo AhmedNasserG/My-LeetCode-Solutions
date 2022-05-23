78d8592a908d6d0762a1df163ddd853aa7543b91
Ahmed Nasser <ahmednasser217217@gmail.com>
2022-05-23 20:13:24 +0200

    8 ms

--- solutions/56. Merge Intervals.java
+++ solutions/56. Merge Intervals.java
@@ -1,27 +1,17 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
         List<int[]> list = new ArrayList();
-        int[] lastInterval = new int[2];
-        lastInterval[0] = intervals[0][0];
-        lastInterval[1] = intervals[0][1];
-        for (int i = 1; i < intervals.length; i++) {
-            if (intersect(lastInterval, intervals[i])) {
-                lastInterval[0] = Math.min(lastInterval[0], intervals[i][0]);
-                lastInterval[1] = Math.max(lastInterval[1], intervals[i][1]);
-            } else {
-                list.add(lastInterval.clone());
-                lastInterval[0] = intervals[i][0];
-                lastInterval[1] = intervals[i][1];
+        for (int[] interval : intervals) {
+            if (!list.isEmpty()) {
+                int[] lastInterval = list.get(list.size() - 1);
+                if (lastInterval[1] >= interval[0]) {
+                    lastInterval[1] = Math.max(lastInterval[1], interval[1]);
+                    continue;
+                }
             }
+            list.add(interval);
         }
-        list.add(lastInterval);
-        return list.toArray(new int[0][]);
-    }
-    
-    private boolean intersect(int[] a, int[] b) {
-        int start = Math.max(a[0], b[0]);
-        int end = Math.min(a[1], b[1]);
-        return start <= end;
+        return list.toArray(new int[0][0]);
     }
 }
