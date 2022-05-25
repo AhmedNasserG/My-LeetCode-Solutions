783583ed0273145db6fa0207c59925abf8a674ed
Ahmed Nasser <ahmednasser217217@gmail.com>
2022-05-25 23:08:00 +0200

    3 ms

--- solutions/4. Median of Two Sorted Arrays.java
+++ solutions/4. Median of Two Sorted Arrays.java
@@ -1,25 +1,30 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n = nums1.length, m = nums2.length;
-        int[] merged = new int[n + m];
-        int i = 0, l = 0, r = 0;
-        while (l < n && r < m) {
-            if (nums1[l] < nums2[r]) {
-                merged[i++] = nums1[l++];
-            } else {
-                merged[i++] = nums2[r++];
+        if (m < n) return findMedianSortedArrays(nums2, nums1);
+        int start = 0, end = n;
+        while (start <= end) {
+            int xPos = (start + end) / 2;
+            int yPos = (n + m + 1) / 2 - xPos;
+            
+            int lX = xPos - 1 < 0 ? Integer.MIN_VALUE : nums1[xPos - 1];
+            int lY = yPos - 1 < 0 ? Integer.MIN_VALUE : nums2[yPos - 1];
+            int rX = xPos < n ? nums1[xPos] : Integer.MAX_VALUE;
+            int rY = yPos < m ? nums2[yPos] : Integer.MAX_VALUE;
+            
+            if (lX <= rY && lY <= rX) {
+                if ((n + m) % 2 == 0) {
+                    return (Math.max(lX, lY) + Math.min(rX, rY)) / 2.0;
+                } else {
+                    return Math.max(lX, lY);
+                }
+            } else if (lX > rY) {
+                end = xPos - 1;
+            } else {
+                start = xPos + 1;
             }
         }
-        while (l < n) {
-            merged[i++] = nums1[l++];
-        }
-        while (r < m) {
-            merged[i++] = nums2[r++];
-        }
-        if ((m + n) % 2 == 0) {
-            return (merged[(m + n) / 2] + merged[(m + n) / 2 - 1]) / 2.0;
-        } else {
-            return merged[(m + n) / 2];
-        }
+        return -1;
     }
 }
+​
