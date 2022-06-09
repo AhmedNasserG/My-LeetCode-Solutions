1d868e4b0c4db2a472f078e02f883d4bd07471c1
Ahmed Nasser <ahmednasser217217@gmail.com>
2022-06-09 21:50:18 +0200

    70 ms

---
+++ solutions/1928. Minimum Cost to Reach Destination in Time.java
@@ -0,0 +1,39 @@
+import java.util.*;
+​
+class Solution {
+    public int minCost(int maxTime, int[][] edges, int[] passingFees) {
+        int n = passingFees.length;
+        ArrayList<int[]>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int t = edge[2];
+            adj[u].add(new int[] {v, t});
+            adj[v].add(new int[] {u, t});
+        }
+        int[] shortestTime = new int[n];
+        Arrays.fill(shortestTime, (int) 1e9);
+        shortestTime[0] = 0;
+        // {v, cost, time}
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            if (a[1] == b[1]) return a[2] - b[2];
+            return a[1] - b[1];
+        });
+        pq.add(new int[] {0, passingFees[0], 0});
+        while (!pq.isEmpty()) {
+            int[] cur = pq.remove();
+            int u = cur[0], cost = cur[1], time = cur[2];
+            if (u == n - 1) return cost;
+            for (int[] nxt : adj[u]) {
+                int v = nxt[0], t = nxt[1], c = passingFees[v];
+                if (time + t > maxTime) continue;
+                if (time + t < shortestTime[v]) {
+                    shortestTime[v] = time + t;
+                    pq.add(new int[] {v, cost + c, shortestTime[v]});
+                }
+            }
+        }
+        return -1;
+    }
+}
