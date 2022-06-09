67fe4052afb2cc635d9ea4cc378ccabb83d763bc
Ahmed Nasser <ahmednasser217217@gmail.com>
2022-06-09 21:52:54 +0200

    Add new solution to problem 1928
    Add dynamic programming solution to 1928. Minimum Cost to Reach Destination in Time

--- solutions/1928. Minimum Cost to Reach Destination in Time.java
+++ solutions/1928. Minimum Cost to Reach Destination in Time.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// Dijkstra
 ​
 class Solution {
     public int minCost(int maxTime, int[][] edges, int[] passingFees) {
@@ -36,4 +36,42 @@ class Solution {
         }
         return -1;
     }
+}
+
+// dp
+class Solution {
+    public int minCost(int maxTime, int[][] edges, int[] passingFees) {
+        this.maxTime = maxTime;
+        this.passingFees = passingFees;
+        int n = passingFees.length;
+        adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int t = edge[2];
+            adj[u].add(new int[] {v, t});
+            adj[v].add(new int[] {u, t});
+        }
+        memo = new int[n][maxTime + 1];
+        for (int a[] : memo) Arrays.fill(a, -1);
+        int ans = dp(0 ,0);
+        if (ans >= INF) return -1;
+        return ans;
+    }
+    static int maxTime;
+    static int INF = (int)1e9;
+    static int[] passingFees;
+    static ArrayList<int[]>[] adj;
+    static int[][] memo;
+    int dp(int u, int curTime) {
+        if (curTime > maxTime) return INF;
+        if (u == passingFees.length - 1) return passingFees[u];
+        if (memo[u][curTime] != -1) return memo[u][curTime];
+        memo[u][curTime] = INF;
+        for (int[] nxt : adj[u]) {
+            memo[u][curTime] = Math.min(memo[u][curTime], passingFees[u] + dp(nxt[0], curTime + nxt[1]));
+        }
+        return memo[u][curTime];
+    }
 }
