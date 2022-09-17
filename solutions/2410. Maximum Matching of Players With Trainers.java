846f5b14eae19ac5bcc27214d54f3beb18c53bf8
Ahmed Nasser <ahmednasser217217@gmail.com>
2022-09-17 18:26:16 +0200

    32 ms

---
+++ solutions/2410. Maximum Matching of Players With Trainers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int ans = 0, i = 0, j = 0;
+        while (i < players.length && j < trainers.length) {
+            if (players[i] <= trainers[j]) {
+                ans++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return ans;
+    }
+}
