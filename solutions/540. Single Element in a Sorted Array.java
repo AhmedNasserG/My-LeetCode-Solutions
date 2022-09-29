4644bb436d2ab7b9544ed7c0fc72cf833f8137ab
Ahmed Nasser <ahmednasser217217@gmail.com>
2022-09-29 13:14:38 +0200

    0 ms

--- solutions/540. Single Element in a Sorted Array.java
+++ solutions/540. Single Element in a Sorted Array.java
@@ -1,15 +1,19 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int start = 0, end = nums.length - 1;
-        while (start < end) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        if (nums[0] != nums[1]) return nums[0];
+        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+        
+        int start = 1, end = n - 2;
+        while(start <= end) {
             int mid = (start + end) / 2;
-            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
+            if (mid % 2 == 1 && nums[mid] == nums[mid - 1] || mid % 2 == 0 && nums[mid] == nums[mid + 1]) {
                 start = mid + 1;
             } else {
-                end = mid;
+                end = mid - 1;
             }
         }
         return nums[start];
     }
 }
-​
