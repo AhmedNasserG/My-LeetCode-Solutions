54c25ca59ca0bf656081e6523dab0589a9c3a224
Ahmed Nasser <ahmednasser217217@gmail.com>
2022-10-06 18:13:51 +0200

    2 ms

--- solutions/169. Majority Element.java
+++ solutions/169. Majority Element.java
@@ -1,6 +1,11 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        int res = nums[0], count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) res = nums[i];
+            if (nums[i] == res) count++;
+            else count--;
+        }        
+        return res;
     }
 }
