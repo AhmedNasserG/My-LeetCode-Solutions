e433884b86835ac0dfdf3352572d51d0e845535f
Ahmed Nasser <ahmednasser217217@gmail.com>
2022-10-19 12:57:52 +0200

    4 ms

--- solutions/4. Median of Two Sorted Arrays.java
+++ solutions/4. Median of Two Sorted Arrays.java
@@ -1,30 +1,33 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length, m = nums2.length;
-        if (m < n) return findMedianSortedArrays(nums2, nums1);
-        int start = 0, end = n;
+       if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+        
+        int x = nums1.length, y = nums2.length;
+        int start = 0, end = x;
         while (start <= end) {
-            int xPos = (start + end) / 2;
-            int yPos = (n + m + 1) / 2 - xPos;
+            int mid = start + (end - start) / 2;
+            int sizeTakenFromX = mid;
+            int sizeTakenFromY = (x + y + 1) / 2 - sizeTakenFromX;
             
-            int lX = xPos - 1 < 0 ? Integer.MIN_VALUE : nums1[xPos - 1];
-            int lY = yPos - 1 < 0 ? Integer.MIN_VALUE : nums2[yPos - 1];
-            int rX = xPos < n ? nums1[xPos] : Integer.MAX_VALUE;
-            int rY = yPos < m ? nums2[yPos] : Integer.MAX_VALUE;
+            int xLeft = sizeTakenFromX == 0 ? Integer.MIN_VALUE : nums1[sizeTakenFromX - 1];
+            int xRight = sizeTakenFromX == nums1.length ? Integer.MAX_VALUE : nums1[sizeTakenFromX];
             
-            if (lX <= rY && lY <= rX) {
-                if ((n + m) % 2 == 0) {
-                    return (Math.max(lX, lY) + Math.min(rX, rY)) / 2.0;
+            int yLeft = sizeTakenFromY == 0 ? Integer.MIN_VALUE : nums2[sizeTakenFromY - 1];
+            int yRight = sizeTakenFromY == nums2.length ? Integer.MAX_VALUE : nums2[sizeTakenFromY];
+            
+            if (xLeft <= yRight && yLeft <= xRight) {
+                if ((x + y) % 2 == 0) {
+                    return (Math.max(xLeft, yLeft) + Math.min(xRight, yRight)) / 2.0;
                 } else {
-                    return Math.max(lX, lY);
+                    return Math.max(xLeft, yLeft);
                 }
-            } else if (lX > rY) {
-                end = xPos - 1;
-            } else {
-                start = xPos + 1;
+            } else if (xLeft > yRight) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
-        return -1;
+        return 0;
     }
 }
 ​
